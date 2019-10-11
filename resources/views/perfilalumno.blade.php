b0d8d2ba9959cdfed543f6aa81fa573726f612a8
frangersal <frangersal@outlook.com>
2019-10-11 15:45:45 -0500

    Se agrego interfaz a los perfiles de tutor alumno y admin

--- resources/views/perfilalumno.blade.php
+++ resources/views/perfilalumno.blade.php
@@ -68,7 +68,7 @@
             
             
             
-            <div class="col-lg"style="background-color:##EEEEFF;">   
+            <div class="col-lg"style="background-color:#EEEEFF;">   
             <div class="row">
                 <div class="col-1"></div>
                     <div class="col-10">
@@ -190,6 +190,8 @@
           <div class="col-1 colright"></div>
         <div class="col-1"style="background-color:#EEEEFF"></div>
     </div> 
+
+
     <div class="row">
         <div class="col-1"style="background-color:#EEEEFF;"></div>
           <div class="col-1 colleft"></div>
@@ -202,22 +204,126 @@
         <div class="col-1"style="background-color:#EEEEFF"></div>
     </div> 
 
-
     <div class="row">
         <div class="col-1"style="background-color:#EEEEFF;"></div>
           <div class="col-1 colleft"></div>
-               
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" ></textarea>
-            </div>
-
-          
+                <div class="col-lg"style="background-color:#fff;">
+                
+                <hr style="border-color:#233D7B;border-width: 2px;">
+                Agregar reunion:
             </div>
           <div class="col-1 colright"></div>
         <div class="col-1"style="background-color:#EEEEFF"></div>
     </div> 
-    
+    <div class="row">
+        <div class="col-1"style="background-color:#EEEEFF;"></div>
+          <div class="col-1 colleft"></div>
+            <div class="form-row col-lg"style="background-color:#fff;">
+                <div class="col-12">
+                <input class="form-control" type="text" placeholder="Default input">
+                </div>
+                
+            </div>
+            <div class="form-row col-lg"style="background-color:#fff;">
+                <div class="btn col-3">
+                <button type="submit" class="btn btn-primary" style="background-color:#233D7B">Agregar</button>
+                </div>
+                <div class="btn col-3">
+                <button type="submit" class="btn btn-primary" style="background-color:#233D7B">Editar</button>
+                </div>
+                <div class="btn col-3">
+                <button type="submit" class="btn btn-primary" style="background-color:#233D7B">Eliminar</button>
+                </div>
+                </div>
+          <div class="col-1 colright"></div>
+        <div class="col-1"style="background-color:#EEEEFF"></div>
+    </div> 
+    <div class="row">
+        <div class="col-1"style="background-color:#EEEEFF;"></div>
+          <div class="col-1 colleft"></div>
+            <div class="col-lg"style="background-color:#fff;">                
+                <textarea class="form-control col-12" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <br>
+            </div>
+          <div class="col-1 colright"></div>
+        <div class="col-1"style="background-color:#EEEEFF"></div>
+    </div> 
+
+    <div class="row">
+        <div class="col-1"style="background-color:#EEEEFF;"></div>
+          <div class="col-1 colleft"></div>
+                <div class="col-lg"style="background-color:#fff;">
+                
+                <hr style="border-color:#233D7B;border-width: 2px;">
+                Agregar reunion:
+            </div>
+          <div class="col-1 colright"></div>
+        <div class="col-1"style="background-color:#EEEEFF"></div>
+    </div> 
+    <div class="row">
+        <div class="col-1"style="background-color:#EEEEFF;"></div>
+          <div class="col-1 colleft"></div>
+            <div class="form-row col-lg"style="background-color:#fff;">
+                <div class="col-12">
+                <input class="form-control" type="text" placeholder="Default input">
+                </div>
+                
+            </div>
+            <div class="form-row col-lg"style="background-color:#fff;">
+                <div class="btn col-3">
+                <button type="submit" class="btn btn-primary" style="background-color:#233D7B">Agregar</button>
+                </div>
+                <div class="btn col-3">
+                <button type="submit" class="btn btn-primary" style="background-color:#233D7B">Editar</button>
+                </div>
+                <div class="btn col-3">
+                <button type="submit" class="btn btn-primary" style="background-color:#233D7B">Eliminar</button>
+                </div>
+                </div>
+          <div class="col-1 colright"></div>
+        <div class="col-1"style="background-color:#EEEEFF"></div>
+    </div> 
+    <div class="row">
+        <div class="col-1"style="background-color:#EEEEFF;"></div>
+          <div class="col-1 colleft"></div>
+            <div class="col-lg"style="background-color:#fff;">                
+                <textarea class="form-control col-12" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <br>
+            </div>
+          <div class="col-1 colright"></div>
+        <div class="col-1"style="background-color:#EEEEFF"></div>
+    </div> 
+    <div class="row">
+        <div class="col-1"style="background-color:#EEEEFF;"></div>
+          <div class="col-1 colleft"></div>
+                <div class="col-lg"style="background-color:#fff;">
+                
+                <hr style="border-color:#233D7B;border-width: 2px;">
+                Agregar foto:
+            </div>
+          <div class="col-1 colright"></div>
+        <div class="col-1"style="background-color:#EEEEFF"></div>
+    </div> 
+    <div class="row">
+        <div class="col-1"style="background-color:#EEEEFF;"></div>
+          <div class="col-1 colleft"></div>
+            <div class="form-row col-lg"style="background-color:#fff;">
+                <div class="col-12">
+                <input class="form-control" type="text" placeholder="Default input">
+                </div>
+                
+            </div>
+            <div class="form-row col-lg"style="background-color:#fff;">
+                <div class="btn col-6">
+                <button type="submit" class="btn btn-primary" style="background-color:#233D7B">Bucar</button>
+                </div>
+                <div class="btn col-6">
+                <button type="submit" class="btn btn-primary " style="background-color:#233D7B">Eliminar</button>
+                </div>
+                </div>
+          <div class="col-1 colright"></div>
+        <div class="col-1"style="background-color:#EEEEFF"></div>
+    </div> 
 
     @include ('includes.footer') 
     </body>
